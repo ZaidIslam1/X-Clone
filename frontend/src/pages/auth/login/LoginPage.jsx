bfc2f50d0a97294fe6cc123be458cfcb4c59cda1
ZaidIslam1 <121275717+ZaidIslam1@users.noreply.github.com>
2025-07-19 01:15:37 -0600

    FeedType Posts working now

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -6,7 +6,6 @@ import XSvg from "../../../components/svgs/X";
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import toast from "react-hot-toast";
 
 const LoginPage = () => {
     const [formData, setFormData] = useState({
@@ -30,7 +29,6 @@ const LoginPage = () => {
                     body: JSON.stringify({ username, password }),
                 });
                 const data = await res.json();
-                console.log(data);
                 if (!res.ok) {
                     throw data.error;
                 }
